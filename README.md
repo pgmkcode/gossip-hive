57463e9da56c9ae5442099bb8c2eeefef7b81889
Pablo Germán Maddalena Kreff <pkreff@linti.unlp.edu.ar>
2026-03-01 20:35:20 +0100

    new features

--- README.md
+++ README.md
@@ -1,20 +1,119 @@
-# gossip-hive
+# Gossip-Hive
 T-Pot as a source of Cyber Threat Intelligence
 
-## Requirements for MISP
-* MISP
-* Exposed ElasticSearch port from T-Pot
-* Debian 12 (tested)
-* Install Python dot-env and activate 
-* Install requirements.txt
-* Assign environment variables (check .env file)
+## Introduction
+Gossip-Hive originated as a project idea developed during my postgraduate studies in Networks and Security, while I was working at [LINTI](https://www.linti.unlp.edu.ar/) (Laboratorio de Investigación en Nuevas Tecnologías Informáticas) at [UNLP](https://unlp.edu.ar/) (Universidad Nacional de La Plata). During that period, I presented the research progress at various conferences and workshops. Final document at [SEDICI](https://sedici.unlp.edu.ar/handle/10915/190822)
 
-#How does Gossip Hive work for MISP?
+Upon completing the postgraduate program (final GPA: 9.33), I have continued to maintain and further develop the project in my spare time.
+
+
+## How does Gossip Hive for MISP work?
 Gossip Hive collects attacks received by T-Pot at a set frequency. The ports where the attacks are received and the IP addresses to be excluded are established (it's recommended to exclude T-Pot's own address). When attacks are reported to MISP, an event is created with the corresponding taxonomies and galaxies, and certain objects of the received attacks are tagged.
 
-## Requirements for IntelOwl Analyzer
-* IntelOwl
-* Exposed ElasticSearch port from T-Pot
+## Gossip-Hive for MISP requirements
+* MISP
+* T-Pot
 
-#How does Gossip Hive work for IntelOwl?
-Simply look for the last attack received for a given IP address.
+## Gossip-Hive features
+- Automatic tags (Galaxies and Taxonomies)
+- Correlation options
+- Destination port, protocol, and date/time of the first attack
+- Suricata alerts and signatures
+- Attack count
+- IP address information (country, ASN name, ASN number, and reputation)
+
+### T-Pot steps
+* Expose the ElasticSearch port from T-Pot (to remember even every time a _custom-docker-compose.yml_ is generated)
+```
+sed -i 's|127.0.0.1:64298:9200|0.0.0.0:64298:9200|' ~/tpotce/docker-compose.yml
+```
+* Get the Elasticsearch API URL and set it at .env 
+```
+ELASTICSEARCH_URL
+```
+* Exclude T-Pot IP address 
+```
+EXCLUDED_SRC_IPS
+```
+
+### MISP steps
+* Get the MISP API KEY and set it at .env
+```
+MISP_KEY
+```
+* Get the MISP API URL and set it at .env 
+```
+MISP_URL
+```
+* Check other parameters if needed
+
+### Gossip-Hive steps
+```
+sudo apt update
+sudo apt install -y python3 python3-venv python3-pip
+python3 -m venv gossip-hive
+source gossip-hive/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+python3 gossip-hive-misp.py
+```
+
+### Gossip-Hive frequency
+```
+python3 gossip-hive-misp.py --minutes=15
+```
+Default frequency is 60 minutes. 
+* Configure a cron job to execute it with the same time parameter.
+
+### Compatible honeypots from T-Pot
+- Adbhoney
+- CitrixHoneypot
+- Ciscoasa
+- ConPot
+- Cowrie
+- Ddospot
+- Dicompot
+- Dionaea
+- ElasticPot
+- Endlessh
+- Go-pot
+- Heralding
+- Honeypots
+- Honeyaml
+- Ipphoney
+- Log4pot
+- Mailoney
+- Medpot
+- Miniprint
+- Tanner
+- Redishoneypot
+- Sentrypeer
+- Wordpot
+
+### Galaxies and taxonomies used in MISP
+- TLP
+- PAP
+- cssa:sharing-class
+- cssa:origin
+- admiralty-scale:source-reliability
+- misp-galaxy:mitre-d3fend
+- misp-galaxy:mitre-attack-pattern
+- honeypot-basic:communication-interface
+- honeypot-basic:data-capture
+- honeypot-basic:interaction-level
+- diamond-model:Infrastructure
+- kill-chain:Exploitation
+- kill-chain:Reconnaissance
+
+### Licenses
+GPLv2: [conpot](https://github.com/mushorg/conpot/blob/master/LICENSE.txt), [galah](https://github.com/0x4D31/galah?tab=Apache-2.0-1-ov-file#readme), [dionaea](https://github.com/DinoTools/dionaea/blob/master/LICENSE), [honeytrap](https://github.com/armedpot/honeytrap/blob/master/LICENSE), [suricata](https://suricata.io/features/open-source/)
+GPLv3: [T-Pot](https://github.com/telekom-security/tpotce?tab=GPL-3.0-1-ov-file#readme), [adbhoney](https://github.com/huuck/ADBHoney), [elasticpot](https://gitlab.com/bontchev/elasticpot/-/blob/master/LICENSE), [ewsposter](https://github.com/telekom-security/ewsposter), [log4pot](https://github.com/thomaspatzke/Log4Pot/blob/master/LICENSE), [fatt](https://github.com/0x4D31/fatt/blob/master/LICENSE), [heralding](https://github.com/johnnykv/heralding/blob/master/LICENSE.txt), [ipphoney](https://gitlab.com/bontchev/ipphoney/-/blob/master/LICENSE), [miniprint](https://github.com/sa7mon/miniprint?tab=GPL-3.0-1-ov-file#readme), [redishoneypot](https://github.com/cypwnpwnsocute/RedisHoneyPot/blob/main/LICENSE), [sentrypeer](https://github.com/SentryPeer/SentryPeer/blob/main/LICENSE.GPL-3.0-only), [snare](https://github.com/mushorg/snare/blob/master/LICENSE), [tanner](https://github.com/mushorg/snare/blob/master/LICENSE)
+Apache 2 License: [cyberchef](https://github.com/gchq/CyberChef/blob/master/LICENSE), [dicompot](https://github.com/nsmfoo/dicompot/blob/master/LICENSE), [elasticsearch](https://github.com/elasticsearch/elasticsearch/blob/master/LICENSE.txt), [go-pot](https://github.com/ryanolee/go-pot?tab=License-1-ov-file#readme), [h0neytr4p](https://github.com/pbssubhash/h0neytr4p?tab=Apache-2.0-1-ov-file#readme), [logstash](https://github.com/elasticsearch/logstash/blob/master/LICENSE.txt), [kibana](https://github.com/elasticsearch/kibana/blob/master/LICENSE.txt), [docker](https://github.com/docker/docker/blob/master/LICENSE)
+MIT license: [autoheal](https://github.com/willfarrell/docker-autoheal?tab=MIT-1-ov-file#readme), [beelzebub](https://github.com/mariocandela/beelzebub?tab=MIT-1-ov-file#readme), [ciscoasa](https://github.com/Cymmetria/ciscoasa_honeypot/blob/master/LICENSE), [ddospot](https://github.com/aelth/ddospot/blob/master/LICENSE), [elasticvue](https://github.com/cars10/elasticvue/blob/master/LICENSE), [glutton](https://github.com/mushorg/glutton/blob/master/LICENSE), [hellpot](https://github.com/yunginnanet/HellPot/blob/master/LICENSE), [honeyaml](https://github.com/mmta/honeyaml?tab=MIT-1-ov-file#readme), [maltrail](https://github.com/stamparm/maltrail/blob/master/LICENSE)
+Unlicense: [endlessh](https://github.com/skeeto/endlessh/blob/master/UNLICENSE)
+Other: [citrixhoneypot](https://github.com/MalwareTech/CitrixHoneypot#licencing-agreement-malwaretech-public-licence), [cowrie](https://github.com/cowrie/cowrie/blob/master/LICENSE.rst), [mailoney](https://github.com/awhitehatter/mailoney), [Elastic License](https://www.elastic.co/licensing/elastic-license), [Wordpot](https://github.com/gbrindisi/wordpot)
+AGPL-3.0: [MISP](https://github.com/MISP/MISP?tab=AGPL-3.0-1-ov-file#readme) [honeypots](https://github.com/qeeqbox/honeypots/blob/main/LICENSE) [IntelOwl](https://github.com/intelowlproject/IntelOwl?tab=AGPL-3.0-1-ov-file#readme)
+[Public Domain (CC)](https://creativecommons.org/publicdomain/zero/1.0/): [Harvard Dataverse](https://dataverse.harvard.edu/dataverse/harvard/?q=dicom)
+
+
+
--- T-Pot to MISP/gossip-hive-misp.py
+++ T-Pot to MISP/gossip-hive-misp.py
@@ -1,7 +1,7 @@
 from pymisp import PyMISP, MISPEvent, MISPObject  
 from datetime import datetime, timedelta,timezone
 from elasticsearch import Elasticsearch
-import json, logging, urllib3, os, argparse
+import json, logging, urllib3, os, argparse, sys
 from dotenv import load_dotenv
 
 parser = argparse.ArgumentParser(description="Optional parameter --minutes")
@@ -47,8 +47,11 @@ minutes_ago = now_local - timedelta(minutes=args.minutes)
 day_yesterday = minutes_ago.day
 
 
-filepath = '/tmp/date_time_limit_range.txt'
+filepath = '/tmp/gossip_hive_date_time_limit_range.txt'
 datetime_limit_range_str = read_last_timestamp(filepath)
+
+with open('./mitre-enterprise-attack-attack-pattern.json', 'r', encoding='utf-8') as f:
+    mitre_enterprise_attack_attack_pattern = json.load(f)
 
 low_limit_range = datetime_limit_range_str if datetime_limit_range_str else f"now-{args.minutes}m"
 if not datetime_limit_range_str:
@@ -59,7 +62,7 @@ high_limit_range = now_utc.strftime('%Y-%m-%dT%H:%M:%S.000Z')
 save_last_timestamp(filepath, high_limit_range)
 
 load_dotenv()
-dest_ports_str = os.getenv('DEST_PORT', '22,80,110,143,443,465,993,995,1080,5432,5900')
+dest_ports_str = os.getenv('DEST_PORT', '21,22,23,25,53,69,80,102,110,123,135,143,161,389,443,445,502,623,631,993,995,1025,1080,1433,1521,1723,1883,1900,2404,2575,3000,3306,3389,4818,5000,5060,5432,5555,5900,6379,6667,8080,8081,8090,8443,9100,9200,10001,11112,11211,25565,44818,47808,50100')
 dest_ports = [int(port.strip()) for port in dest_ports_str.split(',')]
 
 excluded_ips_str = os.getenv('EXCLUDED_SRC_IPS', '127.0.0.1')
@@ -70,32 +73,35 @@ query = {
   "query": {
     "bool": {
       "filter": [
- 
+
         {
           "range": {
             "@timestamp": {
               "gt": low_limit_range,
-              "lte":  high_limit_range
+              "lte": high_limit_range
             }
           }
         },
-     
+
         {
           "terms": {
-            "type.keyword": ["Suricata","Heralding"]
+            "type.keyword": ["Suricata","Heralding","Adbhoney","Ciscoasa","CitrixHoneypot","ConPot","Cowrie","Ddospot","Dicompot","Dionaea","ElasticPot","Endlessh","Go-pot","Honeypots","Honeyaml","Ipphoney","Log4pot","Mailoney","Medpot","Miniprint","Redishoneypot","Sentrypeer","Tanner","Wordpot"]
           }
         },
-          {
-          "terms": {
-            "dest_port": dest_ports
+        {
+          "bool": {
+            "should": [
+              { "terms": { "dest_port": dest_ports } },
+              { "bool": { "must_not": { "exists": { "field": "dest_port" } } } }
+            ],
+            "minimum_should_match": 1
           }
         },
-      
+
         {
           "bool": {
             "must_not": [
-             
-             {"terms": {"src_ip": excluded_ips}}
+              {"terms": {"src_ip": excluded_ips}}
             ]
           }
         }
@@ -103,33 +109,32 @@ query = {
     }
   },
   "aggs": {
-    
+
     "unique_combinations": {
       "composite": {
         "size": 10000,
         "sources": [
           { "src_ip":    { "terms": { "field": "src_ip.keyword"   } } },
           { "type":      { "terms": { "field": "type.keyword"     } } },
-          { "dest_port": { "terms": { "field": "dest_port"        } } }
+          { "dest_port": { "terms": { "field": "dest_port", "missing_bucket": True } } }
         ]
       },
       "aggs": {
-           
+
         "min_timestamp": {
           "min": { "field": "@timestamp" }
         },
-     
+
         "first_hit": {
           "top_hits": {
             "size": 1,
             "sort": [
               { "@timestamp": { "order": "asc" } }
-            ]
-            ,
-            "_source": [    "@timestamp",    "src_ip",    "dest_port",    "type",    "event_type",    "alert.signature"  ]
+            ],
+            "_source": ["@timestamp","src_ip","dest_port","type","event_type","data_type","alert.signature","alert.metadata.mitre_technique_id","geoip.country_name","geoip.as_org","geoip.asn","ip_rep","proto","protocol","connection.protocol","action","info","username","password"]
           }
         },
-       
+
         "sorted_buckets": {
           "bucket_sort": {
             "sort": [
@@ -146,27 +151,44 @@ query = {
 
 
 
-es = Elasticsearch(os.getenv('ELASTICSEARCH_URL'))
+
+
+es = Elasticsearch(
+    os.getenv('ELASTICSEARCH_URL'),
+    request_timeout=int(os.getenv('ELASTICSEARCH_TIMEOUT', 30)),
+    max_retries=int(os.getenv('ELASTICSEARCH_MAX_RETRIES', 10)),
+    retry_on_timeout=os.getenv('ELASTICSEARCH_RETRY_ON_TIMEOUT', 'True').lower() == 'true'
+
+)
 MISP_URL = os.getenv("MISP_URL")
 MISP_KEY = os.getenv("MISP_KEY")
 MISP_VERIFY_CERT = os.getenv("MISP_VERIFY_CERT", "False").lower() == "true"
+MISP_DISABLE_CORRELATION_SRC_IP = os.getenv('MISP_DISABLE_CORRELATION_SRC_IP', 'False').lower() == 'true'
+MISP_DISABLE_CORRELATION_DST_PORT = os.getenv('MISP_DISABLE_CORRELATION_DST_PORT', 'False').lower() == 'true'
+MISP_DISABLE_CORRELATION_START_TIME = os.getenv('MISP_DISABLE_CORRELATION_START_TIME', 'False').lower() == 'true'
+MISP_DISABLE_CORRELATION_PROTOCOL = os.getenv('MISP_DISABLE_CORRELATION_PROTOCOL', 'False').lower() == 'true'
+MISP_ENABLE_COMMENTS_IP = os.getenv('MISP_ENABLE_COMMENTS_IP', 'True').lower() == 'true'
+MISP_ENABLE_COMMENTS_SURICATA = os.getenv('MISP_ENABLE_COMMENTS_SURICATA', 'True').lower() == 'true'
+MISP_ENABLE_COMMENTS_HONEYPOTS = os.getenv('MISP_ENABLE_COMMENTS_HONEYPOTS', 'True').lower() == 'true'
 
-ts_file = '/tmp/last_datetime_event.txt'
-last_ts = read_last_timestamp(ts_file)
+MISP_EVENT_PAP = os.getenv('MISP_EVENT_PAP', 'GREEN')
+MISP_EVENT_TLP = os.getenv('MISP_EVENT_TLP', 'green')
 
 evt_time_str = now_local.strftime('%Y-%m-%d %H:%M')
-
-
 
 all_buckets = []
 
 resp = search_logs(es,log_today,query)
 
+if resp is None:
+    print(f"ERROR: Could not retrieve data from {log_today}. Exiting.")
+    sys.exit(1)
+
 if day_today != day_yesterday:
-  resp_yesterday = search_logs(es, log_yesterday, query)
-  all_buckets.extend(
-        resp_yesterday['aggregations']['unique_combinations']['buckets']
-    )
+    resp_yesterday = search_logs(es, log_yesterday, query)
+    all_buckets.extend(
+          resp_yesterday['aggregations']['unique_combinations']['buckets']
+      )
 
 all_buckets.extend(
     resp['aggregations']['unique_combinations']['buckets']
@@ -186,42 +208,244 @@ if all_buckets:
         'admiralty-scale:source-reliability="b"',
         'cssa:origin=honeypot',
         'cssa:sharing-class=unvetted',
-        'PAP:GREEN',
-        'tlp:green',
+        f'PAP:{MISP_EVENT_PAP}',
+        f'tlp:{MISP_EVENT_TLP}',
         'honeypot-basic:communication-interface=network-interface',
         'honeypot-basic:data-capture=attacks',
-        'honeypot-basic:interaction-level=low',
         'misp-galaxy:mitre-d3fend="Decoy Network Resource"',
         'misp-galaxy:mitre-d3fend="Standalone Honeynet"'
     ]:
         misp.tag(evt, tag)
 
+    def handle_suricata(hit, src_attr, obj):
+        obj.add_attribute('protocol', value=hit['proto'], disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+        if MISP_ENABLE_COMMENTS_SURICATA:
+          obj.comment = f"First attack of {bucket['doc_count']} against {hit['type']}"
+        if hit.get('event_type') == 'alert' and 'alert' in hit:
+            if MISP_ENABLE_COMMENTS_SURICATA:
+                sig = hit['alert'].get('signature')
+                if sig:
+                    obj.comment += f" (Alert): {sig}"
+            mitre_technique_id = hit.get('alert', {}).get('metadata', {}).get('mitre_technique_id')
+            if mitre_technique_id and mitre_technique_id in mitre_enterprise_attack_attack_pattern:
+                src_attr.add_tag(f'misp-galaxy:mitre-attack-pattern={mitre_enterprise_attack_attack_pattern[mitre_technique_id]}')
+        else:
+            src_attr.add_tag('misp-galaxy:mitre-attack-pattern="Active Scanning - T1595"')
+            src_attr.add_tag('kill-chain:Reconnaissance')
+
+
+    def handle_adbhoney(hit, src_attr, obj):
+        src_attr.add_tag('honeypot-basic:interaction-level=low')
+        obj.add_attribute('protocol', value='ADB', disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+    
+    def handle_ciscoasa(hit, src_attr, obj):
+        src_attr.add_tag('honeypot-basic:interaction-level=low')
+        obj.add_attribute('protocol', value='HTTPS',disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+
+    def handle_citrixhoneypot(hit, src_attr, obj):
+        src_attr.add_tag('honeypot-basic:interaction-level=low')
+        obj.add_attribute('protocol', value='HTTP', disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+
+    def handle_conpot(hit, src_attr, obj):
+        src_attr.add_tag('honeypot-basic:interaction-level=low')
+        obj.add_attribute('protocol', value=hit['data_type'].upper().replace('_', ' '), disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+
+    def handle_cowrie(hit, src_attr, obj):
+        src_attr.add_tag('honeypot-basic:interaction-level=medium')
+        port_protocols_cowrie = {
+            22: 'SSH',
+            23: 'TELNET'              
+        }
+        if hit['dest_port'] in port_protocols_cowrie:
+            obj.add_attribute('protocol', value=port_protocols_cowrie[hit['dest_port']], disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+
+    def handle_ddospot(hit, src_attr, obj):
+        src_attr.add_tag('honeypot-basic:interaction-level=low')
+        port_protocols_ddospot = {
+            19: 'CHARGEN',
+            53: 'DNS',
+            123: 'NTP',
+            1900: 'SSDP'
+        }
+        if hit['dest_port'] in port_protocols_ddospot:
+            obj.add_attribute('protocol', value=port_protocols_ddospot[hit['dest_port']], disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+
+    def handle_dicompot(hit, src_attr, obj):
+        src_attr.add_tag('honeypot-basic:interaction-level=low')
+        obj.add_attribute('protocol', value='DICOM',disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+
+    def handle_dionaea(hit, src_attr, obj):
+        src_attr.add_tag('honeypot-basic:interaction-level=low')
+        protocold = hit['connection']['protocol'].upper()
+        if protocold.endswith('D'):
+            protocold = protocold[:-1]
+        obj.add_attribute('protocol', value=protocold, disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+
+    def handle_elasticpot(hit, src_attr, obj):
+        src_attr.add_tag('honeypot-basic:interaction-level=low')
+        obj.add_attribute('protocol', value='ELASTICSEARCH', disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+        if hit['event_type'] == 'Scan' or hit['event_type'] == 'Head scan':
+            src_attr.add_tag('kill-chain:Reconnaissance')
+        if hit['event_type'] == 'Exploit':
+            src_attr.add_tag('kill-chain:Exploitation')
+
+    def handle_endlessh(hit, src_attr, obj):
+        src_attr.add_tag('honeypot-basic:interaction-level=low')
+        obj.add_attribute('protocol', value='SSH', disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+
+    def handle_gopot(hit, src_attr, obj):
+        src_attr.add_tag('honeypot-basic:interaction-level=low')
+        obj.add_attribute('protocol', value='HTTP', disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+
+    def handle_heralding(hit, src_attr, obj):
+        src_attr.add_tag('misp-galaxy:mitre-attack-pattern="Brute Force - T1110"')
+        src_attr.add_tag('kill-chain:Exploitation')
+        src_attr.add_tag('honeypot-basic:interaction-level=low')
+        obj.add_attribute('protocol', value=hit['proto'].upper(), disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+
+    def handle_honeypots(hit, src_attr, obj):
+        src_attr.add_tag('honeypot-basic:interaction-level=low')
+        obj.add_attribute('protocol', value=hit['protocol'].upper().replace('_', ' '), disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+        if hit['action'] == 'login':
+            src_attr.add_tag('misp-galaxy:mitre-attack-pattern="Brute Force - T1110"')
+            src_attr.add_tag('kill-chain:Exploitation')           
+        if hit['action'] == 'connection':
+            src_attr.add_tag('misp-galaxy:mitre-attack-pattern="Active Scanning - T1595"')
+            src_attr.add_tag('kill-chain:Reconnaissance')
+
+    def handle_honeyaml(hit, src_attr, obj):
+        src_attr.add_tag('honeypot-basic:interaction-level=low')
+        obj.add_attribute('protocol', value='HTTP', disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+
+    def handle_ipphoney(hit, src_attr, obj):
+        src_attr.add_tag('honeypot-basic:interaction-level=low')
+        obj.add_attribute('protocol', value='IPP', disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+
+    def handle_log4pot(hit, src_attr, obj):
+        src_attr.add_tag('honeypot-basic:interaction-level=low')
+        port_protocols_log4pot = {
+            80: 'HTTP',
+            443: 'HTTPS',
+            8080: 'HTTP',
+            9200: 'ELASTICSEARCH',
+            25565: 'HTTP'
+        }
+        if hit['dest_port'] in port_protocols_log4pot:
+            obj.add_attribute('protocol', value=port_protocols_log4pot[hit['dest_port']], disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+
+    def handle_mailoney(hit, src_attr, obj):
+        src_attr.add_tag('honeypot-basic:interaction-level=low')
+        obj.add_attribute('protocol', value='SMTP', disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+
+    def handle_medpot(hit, src_attr, obj):
+        src_attr.add_tag('honeypot-basic:interaction-level=low')
+        obj.add_attribute('protocol', value='HL7', disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+
+    def handle_miniprint(hit, src_attr, obj):
+        src_attr.add_tag('honeypot-basic:interaction-level=low')
+        obj.add_attribute('protocol', value='JETDIRECT', disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+
+    def handle_redishoneypot(hit, src_attr, obj):
+        src_attr.add_tag('honeypot-basic:interaction-level=low')
+        obj.add_attribute('protocol', value='REDIS', disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+
+    def handle_sentrypeer(hit, src_attr, obj):
+        src_attr.add_tag('honeypot-basic:interaction-level=low')
+        obj.add_attribute('protocol', value='SIP', disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+
+    def handle_tanner(hit, src_attr, obj):
+        src_attr.add_tag('honeypot-basic:interaction-level=low')
+        obj.add_attribute('protocol', value='HTTP', disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+
+    def handle_wordpot(hit, src_attr, obj):
+        src_attr.add_tag('honeypot-basic:interaction-level=low')
+        if 'info' in hit and hit['info'] == 'enumeration':
+            src_attr.add_tag('misp-galaxy:mitre-attack-pattern="Active Scanning - T1595"')
+            src_attr.add_tag('kill-chain:Reconnaissance')
+        elif 'username' in hit or 'password' in hit:
+            src_attr.add_tag('misp-galaxy:mitre-attack-pattern="Brute Force - T1110"')
+            src_attr.add_tag('kill-chain:Exploitation')
+        obj.add_attribute('protocol', value='HTTP', disable_correlation=MISP_DISABLE_CORRELATION_PROTOCOL)
+        
+
+    HONEYPOT_HANDLERS = {
+        'Suricata': handle_suricata,
+        'Adbhoney': handle_adbhoney,
+        'CitrixHoneypot': handle_citrixhoneypot,
+        'Ciscoasa': handle_ciscoasa,
+        'ConPot': handle_conpot,
+        'Cowrie': handle_cowrie,
+        'Ddospot': handle_ddospot,
+        'Dicompot': handle_dicompot,
+        'Dionaea': handle_dionaea,
+        'ElasticPot': handle_elasticpot,
+        'Endlessh': handle_endlessh,
+        'Go-pot': handle_gopot,
+        'Heralding': handle_heralding,
+        'Honeypots': handle_honeypots,
+        'Honeyaml': handle_honeyaml,
+        'Ipphoney': handle_ipphoney,
+        'Log4pot': handle_log4pot,
+        'Mailoney': handle_mailoney,
+        'Medpot': handle_medpot,
+        'Miniprint': handle_miniprint,
+        'Tanner': handle_tanner,
+        'Redishoneypot': handle_redishoneypot,
+        'Sentrypeer': handle_sentrypeer,
+        'Wordpot': handle_wordpot
+    }
+
     for bucket in all_buckets:
         hit = bucket['first_hit']['hits']['hits'][0]['_source']
         obj = MISPObject('network-traffic')
-        src_attr = obj.add_attribute('src_ip', value=hit['src_ip'])
-        dst_attr = obj.add_attribute('dst_port', value=hit['dest_port'])
-        obj.comment = f"Source: {hit['type']}; Timestamp: {hit['@timestamp']}"
+        comment = None
+        if MISP_ENABLE_COMMENTS_IP:
+            comment = ' | '.join(
+                filter(
+                    None,
+                    [
+                        hit.get('geoip', {}).get('country_name'),
+                        f"ASN: {hit.get('geoip', {}).get('asn')}"
+                        if hit.get('geoip', {}).get('asn')
+                        else None,
+                        hit.get('geoip', {}).get('as_org'),
+                        hit.get('ip_rep'),
+                    ],
+                )
+            ) or None
 
+        src_attr = obj.add_attribute(
+            'src_ip',
+            value=hit['src_ip'],
+            disable_correlation=MISP_DISABLE_CORRELATION_SRC_IP,
+            comment=comment,
+        )
+        if hit.get('dest_port'): 
+          dst_attr = obj.add_attribute('dst_port', value=hit['dest_port'], disable_correlation=MISP_DISABLE_CORRELATION_DST_PORT)
         
+ 
+        
+        obj.add_attribute('start_time', value=hit['@timestamp'], disable_correlation=MISP_DISABLE_CORRELATION_START_TIME)
+        src_attr.add_tag('diamond-model:Infrastructure')  
 
-        if hit.get('event_type') == 'alert' and 'alert' in hit:
-            sig = hit['alert'].get('signature', 'unknown')
-            obj.comment += f"; Alert - Signature: {sig}"
-            
-        src_attr.add_tag('diamond-model:Infrastructure')
 
-        if hit['type'] == 'Heralding':
-            src_attr.add_tag('misp-galaxy:mitre-attack-pattern="Brute Force - T1110"')
-            src_attr.add_tag('kill-chain:Exploitation')
-        if hit['type'] == 'Suricata' and hit.get('event_type') != 'alert':
-            src_attr.add_tag('misp-galaxy:mitre-attack-pattern="Active Scanning - T1595"')
-            src_attr.add_tag('kill-chain:Reconnaissance')
-    
+        if MISP_ENABLE_COMMENTS_HONEYPOTS and hit['type'] != 'Suricata':
+          obj.comment = f"First attack of {bucket['doc_count']} against {hit['type']}"
+
+
+        handler = HONEYPOT_HANDLERS.get(hit['type'])
+        if handler:
+            handler(hit, src_attr, obj)        
+
+
         misp.add_object(evt, obj)
+
 
     publish_event = os.getenv('MISP_PUBLISH', 'true').lower() == 'true'
 
     if publish_event:
         misp.publish(eid)
+
+
+    
 
